78a218f68e4e71cc77df19c99b9075b671d9b858
Sascha Mckayla <mckaylasascha@gmail.com>
2026-04-16 23:55:10 +0300

    adding click button

--- index.js
+++ index.js
@@ -2,7 +2,12 @@
 const weatherApi = "https://api.weather.gov/alerts/active?area="
 
 // Your code here!
+//Creating the elements
 const input = document.getElementById("state-input");
 const button = document.getElementById("fetch-alerts");
 const display = document.getElementById("alerts-display");
 const errorDiv = document.getElementById("error-message");
+
+//Adding click button
+button.addEventListener("click", () => {
+    
