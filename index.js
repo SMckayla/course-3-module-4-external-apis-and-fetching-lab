dd593299845075b99e17b25270157609010edd9d
Sascha Mckayla <mckaylasascha@gmail.com>
2026-04-16 23:53:17 +0300

    creating the elements

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 const weatherApi = "https://api.weather.gov/alerts/active?area="
 
 // Your code here!
+const input = document.getElementById("state-input");
+const button = document.getElementById("fetch-alerts");
+const display = document.getElementById("alerts-display");
+const errorDiv = document.getElementById("error-message");
